12af331eae0185e1fc152b5bc72018ccf18671a7
haidarakhs <haidarakbar.hs@gmail.com>
2024-10-22 08:42:42 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hrshop/Pages/CartPage.dart';
 import 'package:hrshop/Pages/ItemPage.dart';
 import 'package:hrshop/pages/HomePage.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  DevicePreview(
+    enabled: true, 
+    builder: (context) => MyApp(), 
+  ),
+);
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,10 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
       ),
+      useInheritedMediaQuery: true, 
+      locale: DevicePreview.locale(context), 
+      builder: DevicePreview.appBuilder, 
       routes: {
         "/" : (context) => HomePage(),
         "cartPage" : (context) => CartPage(),
-         "itemPage" : (context) => ItemPage(),
+        "itemPage" : (context) => ItemPage(),
       },
     );
   }
